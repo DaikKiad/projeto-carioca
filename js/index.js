616f0f70bf988988a6d0f17dc341d656d481df2f
Reti <33784173+DaikKiad@users.noreply.github.com>
2023-10-24 09:59:56 -0300

    mudanças

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+const port = 3000;
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
+app.get('/hello', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(3000)
+app.listen(port, function(){
+    console.info(`App rodando em http://localhost:${port}`);
+});
