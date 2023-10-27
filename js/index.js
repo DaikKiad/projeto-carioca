f5599f378840c5da8a5f1c1f59559a8a2e3bd004
Reti <33784173+DaikKiad@users.noreply.github.com>
2023-10-27 20:46:49 -0300

    endpoints usuarios

--- js/index.js
+++ js/index.js
@@ -7,6 +7,31 @@ app.get('/hello', (req, res) => {
   res.send('hello world');
 });
 
+/*
+Crud Usuarios
+get /usuarios - retorna os usuarios
+get /usuario/id - retorna um usuario por id
+post /usuarios - coloca um usuario novo
+put /usuarios/id - atualiza um usuario pelo id
+delete /usuarios/id - remover um usuario pelo id
+*/
+
+const usuarios = [
+  "Primeiro usuario",
+  "Segundo usuario"
+];
+
+/* get /usuarios - retorna os usuarios */
+app.get('/usuarios', (req, res) => {
+  res.send(usuarios);
+});
+
+/*get /usuario/id - retorna um usuario por id */
+app.get('/usuarios/:id', (req, res) => {
+  const id = req.params.id - 1,
+  const mensagem = [id];
+});
+
 app.listen(port, function(){
     console.info(`App rodando em http://localhost:${port}`);
 });
