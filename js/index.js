d4f735ac880637413aeea9154e967d3e45bb1faa
Reti <33784173+DaikKiad@users.noreply.github.com>
2023-10-27 21:19:22 -0300

    começo dos endpoints api

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
+
+const bodyParser = require('body-parser');
+
 var app = express();
 
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
   res.send('hello world');
@@ -26,10 +31,19 @@ app.get('/usuarios', (req, res) => {
   res.send(usuarios);
 });
 
-/*get /usuario/id - retorna um usuario por id */
+/* get /usuario/id - retorna um usuario por id */
 app.get('/usuarios/:id', (req, res) => {
-  const id = req.params.id - 1,
+  const id = req.params.id - 1;
   const mensagem = [id];
+});
+
+/* post /usuarios - coloca um usuario novo */
+app.post('/usuarios', (req, res) => {
+    const usuario = req.body;
+
+    console.log(usuario);
+
+    res.send('Criar um novo usuario.')
 });
 
 app.listen(port, function(){
