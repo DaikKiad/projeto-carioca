aa596d20ea4e88c4da4091e74e744aa3d96378da
Reti <33784173+DaikKiad@users.noreply.github.com>
2023-11-02 04:16:38 -0300

    colocação da api do maps mapa

--- js/apiserver.js
+++ js/apiserver.js
@@ -12,6 +12,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+function initMap() {
+            const mapOptions = {
+                center: { lat: -34.397, lng: 150.644 }, // Coordenadas iniciais do mapa
+                zoom: 15, // Nível de zoom inicial
+            };
+            const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            // Adicione código aqui para configurar a funcionalidade de seleção de lugares próximos.
+            // Você pode adicionar marcadores, círculos, etc., para representar lugares próximos e interagir com eles.
+
+            // Exemplo de adição de um marcador:
+            const marker = new google.maps.Marker({
+                position: { lat: -34.397, lng: 150.644 }, // Coordenadas do marcador
+                map: map, // Mapa onde o marcador deve ser exibido
+                title: "Lugar de interesse", // Título do marcador
+            });
+
+            // Exemplo de pesquisa de lugares próximos:
+            const service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(
+                {
+                    location: mapOptions.center,
+                    radius: 1000, // Raio de busca em metros
+                    type: ["restaurant", "store"], // Tipos de lugares a serem pesquisados
+                },
+                (results, status) => {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        // Processar os resultados da pesquisa aqui
+                    }
+                }
+            );
+        }
+
+        // Chamar a função initMap depois de ter carregado o código da API
+        window.addEventListener('load', function () {
+            initMap();
+        });
+
 // Conectar ao banco de dados MySQL
 const db = mysql.createConnection({
     host: 'localhost',
